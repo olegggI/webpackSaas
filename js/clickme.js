840651c6e5e2b072c544107a529e3512e8f972aa
olegggI <ivanyuk@list.ru>
2016-03-05 19:38:19 +0200

    bower touchswip

--- js/clickme.js
+++ js/clickme.js
@@ -1,6 +1,21 @@
-$('.clickme').on('click', function(e){
+$('.clickme, .first').on('click', function(e){
 	require.ensure([], function(require) {
     	var a = require("clickme.scss");
     	console.log('clicked!');
 	})
+});
+
+
+
+$('.first').swipe({
+	tap: function(event, target){
+		event.stopPropagation();
+		alert(1);
+	}
+});
+
+$('.third').swipe({
+	tap: function(event, target){
+		alert(4);
+	}
 });
